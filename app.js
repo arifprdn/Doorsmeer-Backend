1fb0a834e5bd923f58a82eb308d91e12cb44c525
arifprdn <arifpradana315@gmail.com>
2024-11-26 07:41:28 +0700

    deploy

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express();
-const ngrok = require("ngrok");
 const PORT = 3000;
 const cors = require("cors");
 
@@ -13,16 +12,4 @@ app.use(express.urlencoded({ extended: false }));
 
 app.listen(PORT, async () => {
   console.log(`Server is running on http://localhost:${PORT}`);
-
-  // Inisialisasi Ngrok saat server mulai
-  try {
-    const url = await ngrok.connect({
-      addr: PORT, // Port lokal server Express
-      authtoken: process.env.NGROK_AUTHTOKEN,
-    });
-    console.log(`Ngrok tunnel opened at: ${url}`);
-    console.log(`You can access the server via: ${url}`);
-  } catch (error) {
-    console.error("Error connecting Ngrok:", error);
-  }
 });
